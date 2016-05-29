605e066e2cc2b511d7a0491993d67769ac8239a8
regina <1513898@stud.hs-mannheim.de>
2016-05-29 15:58:45 +0200

    server2.js, neue server datei, task3.1, regina

---
+++ Meilenstein3/server2.js
@@ -0,0 +1,31 @@
+//-----Server-----
+
+var express = require('express');
+var app = express();
+
+var hostname = '127.0.0.1';
+var port = 3000;
+
+app.get('/', function (req, res) {
+  res.send('Hello Regi!');
+});
+
+var playersAll = require('./generated.json');
+
+app.get('/api/players', function(req, res) {
+	var query = req.param('favorites') || 'false';
+	
+
+	if(query === 'true'){
+		res.json(200, playersAll);
+	} else if(query === 'false'){
+		res.json(200, playersAll);
+	} else {
+		res.json(404, { message: 'FAIL' });
+	}
+	
+});
+
+app.listen(port, hostname, function () {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
